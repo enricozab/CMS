c578edf4bb5531074b9bcb33097c86230b57c02c
Thea Firaza <thea_firaza@dlsu.edu.ph>
2018-10-22 13:41:32 +0800

    Update php/faculty-home.php

--- php/faculty-home.php
+++ php/faculty-home.php
@@ -53,7 +53,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-8">
-                    <h3 class="page-header">Dashboard</h3>
+                    <h3 class="page-header">Notifications</h3>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
