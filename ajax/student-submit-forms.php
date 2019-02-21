b6de785020f0cce2767806d5c6644f0ffb68a15d
Thea Firaza <thea_firaza@dlsu.edu.ph>
2019-02-21 18:53:10 +0800

    Update student-submit-forms.php

--- ajax/student-submit-forms.php
+++ ajax/student-submit-forms.php
@@ -2,6 +2,11 @@
   session_start();
   require_once('../mysql_connect.php');
 
+  $query="UPDATE CASES SET IF_NEW=1, STATUS_ID=2, REMARKS_ID=3 WHERE CASE_ID = {$_POST['caseID']}";
+  $result=mysqli_query($dbc,$query);
+  if(!$result){
+    echo mysqli_error($dbc);
+  }
 
   if ($_POST['admission'] == "Full Admission") {
     $query="UPDATE CASES SET ADMISSION_TYPE_ID=1 WHERE CASE_ID = {$_POST['caseID']}";
