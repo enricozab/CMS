9f4abd8703b02119d3383bd116880769ed43cbf9
Carlos Garcia <carlos_martin_garcia@dlsu.edu.ph>
2019-03-01 07:50:22 +0800

    added calendar folder

---
+++ calendar/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '969100577046-a6fgg6qeha0e1svvordjer6l4eib0vub.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'dunmBrNKDJKsvrTuJYAauVYk');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/calendar/google-login.php');
+
+?>
