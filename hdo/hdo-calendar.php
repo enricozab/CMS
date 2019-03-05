9a7bc7b4476c0d7f69f1fc63045916270b51a645
Carlos Garcia <carlos_martin_garcia@dlsu.edu.ph>
2019-03-05 16:08:32 +0800

    Update hdo-calendar.php

--- hdo/hdo-calendar.php
+++ hdo/hdo-calendar.php
@@ -59,12 +59,7 @@
                 </div>
                 <div>
                   <?php
-                    //if(isset($_SESSION['access_token_calendar'])) {
-                      //header('Location: ../calendar/home.php');
-                    //}
-                    //else {
                       include '../calendar/google-login.php';
-                    //}
                   ?>
                   <br><iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=hdo.cms%40gmail.com&amp;color=%231B887A&amp;ctz=Asia%2FManila" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
                 </div>
