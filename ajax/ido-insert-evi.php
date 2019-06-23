9d9015823aaa8ae2e4e1ab4e77f275e737afa06b
Thea Firaza <thea_firaza@dlsu.edu.ph>
2019-06-23 23:35:30 +0800

    y

--- ajax/ido-insert-evi.php
+++ ajax/ido-insert-evi.php
@@ -11,7 +11,6 @@
   else {
     $newQry = "INSERT INTO SUBMITTED_EVIDENCE (SUBMITTED_BY_ID, UPLOADED_BY_NAME, EVIDENCE_TYPE, IDO_ID)
                     VALUES ({$_POST['submittedBy']}, '{$_POST['sName']}', {$_POST['evidence']}, '{$_POST['idoID']}');";
-
   }
   
   $newResult = mysqli_query($dbc,$newQry);
